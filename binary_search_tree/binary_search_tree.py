dd8d3be22428d6a253b7de11e57bcde2e526ffab
drewgoenner <52584951+drewgoenner@users.noreply.github.com>
2020-01-29 14:29:08 -0600

    finished coding insert, haven't tested yet

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -12,7 +12,21 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        # if value is greater or equal to the value of the node, then if there is nothing on the right branch, the leaf becomes the right branch
+        if value >= self.value:
+            if self.right is None:
+                self.right = BinarySearchTree(value)
+        #otherwise, run insert again on the leaf to the right
+            else:
+                self.right.insert(value)
+        # if the value is less than the value of the node, if there isn't anything on the left branch, the leaf becomes the left branch
+        elif value < self.value:
+            if self.left is None:
+                self.left = BinarySearchTree(value)
+        # otherwise, run insert again on the leaf to the left
+            else:
+                self.left.insert(value)
+        
 
     # Return True if the tree contains the value
     # False if it does not
